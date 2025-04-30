ea53a8f5cfed4a39a0f15cc60494c961af509cef
AntoniIwan <antoniiwan02@gmail.com>
2025-04-30 11:33:32 +0200

    Described net_sockets.py and shared_globals.py

--- common/shared_globals.py
+++ common/shared_globals.py
@@ -1,2 +1,4 @@
-MULTICAST_PORT = 8000
-MULTICAST_IP = "224.0.0.1"
+# Multicast configuration constants
+
+MULTICAST_PORT = 8000           # Port number used for multicast communication
+MULTICAST_IP = "224.0.0.1"      # IP address of the multicast group (in the local multicast range)
